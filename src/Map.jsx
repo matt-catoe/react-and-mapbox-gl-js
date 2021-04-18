13e5d747a85a76958373b57e91e14395c6275426
Matt Catoe <matt.catoe.dev@gmail.com>
2021-04-18 14:07:31 -0400

    Add base map

---
+++ src/Map.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+
+mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken = process.env.REACT_APP_ACCESS_TOKEN;
+
+const Map = () => {
+  const mapContainer = useRef();
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    return () => map.remove();
+  }, []);
+  return <div className="map-container" ref={mapContainer} />;
+};
+
+export default Map;
